d446425b0f2161e05d0f4bf7c760249237350377
babysampson <babysampson@gmail.com>
2009-08-03 16:40:19 -0400

    removed red bg color from client pages

--- static/js/admin_overlay.js
+++ static/js/admin_overlay.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("body").css("background","red");
+    //$("body").css("background","red");
 	 
 	$("#slide_properties td.activity label").click(function(){
 		$(this).toggleClass("selected")
