b08b4d7309ad03360a9356e4ae14ffe1027fa1a9
broderboy <timothy.broder@gmail.com>
2009-12-09 16:00:01 -0500

    changed suds to SOAPpy

--- stager/jira/views.py
+++ stager/jira/views.py
@@ -9,8 +9,8 @@ from django.http import HttpResponseRedirect, Http404
 from django.views.static import serve
 import re
 from stager.settings import JIRA_USER, JIRA_PASS, JIRA_WSDL
-import getpass, datetime, array, base64
-from suds.client import Client as SoapClient, WebFault
+import SOAPpy, getpass, datetime, array, base64
+from SOAPpy import Types
 from stager.jira.decorators import *
 from django.core.files.uploadedfile import SimpleUploadedFile
 
@@ -42,18 +42,19 @@ def view_project(request, client_path, project_path, jira_key):
         client = Client.objects.get(path=client_path)
         project = client.projects.get(path=project_path)
 
-        soap = SoapClient(JIRA_WSDL)
+        soap = SOAPpy.WSDL.Proxy(JIRA_WSDL)
         auth = request.session.get('jira_auth')
 
         jira_project = JiraProject.objects.get(key=jira_key)
-        jiras = soap.service.getIssuesFromFilterWithLimit(auth, "10312", 0, 999)
-
-        for jira in jiras[0]:
-
+        jiras = soap.getIssuesFromFilterWithLimit(auth, "10312", 0, 999)
+        
+        for jira in jiras:
             test = jira.key.split('-')
             if len(test[1]) == 1:
-                pad = "00%s" % test[1]
+                pad = "000%s" % test[1]
             elif len(test[1]) == 2:
+                pad = "00%s" % test[1]
+            elif len(test[1]) == 3:
                 pad = "0%s" % test[1]
             else:
                 pad = test[1]
@@ -64,12 +65,12 @@ def view_project(request, client_path, project_path, jira_key):
         
         return render_to_response('jira_project.html', {'project':project, 
                                                                    'client':client,
-                                                                   'jiras':jiras[0], 
+                                                                   'jiras':jiras, 
                                                                    'jira_project': jira_project,
                                                                    'user':request.user,
                                                                    'client_path': client_path,
                                                                    'project_path': project_path})
-    except Project.DoesNotExist:
+    except Client.DoesNotExist, Project.DoesNotExist:
         raise Http404
     
 @login_required
@@ -79,7 +80,7 @@ def insert_issue(request, client_path, project_path, jira_key):
         client = Client.objects.get(path=client_path)
         project = client.projects.get(path=project_path)
 
-        soap = SoapClient(JIRA_WSDL)
+        soap = SOAPpy.WSDL.Proxy(JIRA_WSDL)
         auth = request.session.get('jira_auth')
 
         jira_project = JiraProject.objects.get(key=jira_key)
@@ -90,7 +91,7 @@ def insert_issue(request, client_path, project_path, jira_key):
             if form.is_valid():
                 submitted = True
 
-                newissue = soap.service.createIssue(auth, {'project': jira_project.key, 
+                newissue = soap.createIssue(auth, {'project': jira_project.key, 
                                                    'type': '1', 
                                                    'description': form.cleaned_data['description'],
                                                    'summary': form.cleaned_data['summary'],
@@ -105,7 +106,7 @@ def insert_issue(request, client_path, project_path, jira_key):
                                              form.cleaned_data['attachment'].read())
     
                     b64t = base64.encodestring(uploaded.read())
-                    soap.serviceaddBase64EncodedAttachmentsToIssue(auth,
+                    soap.addBase64EncodedAttachmentsToIssue(auth,
                                                newissue['key'],
                                                [filename],
                                                [b64t])
