3cc93495807d878b22fd84351fb91d3e561516cd
Malka R <mmr@alexanderinteractive.com>
2011-10-24 11:42:47 -0400

    switch to unobtrusive javascript

--- static/js/project.js
+++ static/js/project.js
@@ -5,8 +5,10 @@ function show(id_to_show, id_to_hide)
 	$(id_to_show).show();
 	$(id_to_hide).hide();
 	}
-
-
+function selectSection(value){
+	VAR_CURRENT_SECTION = value;
+	gotosection(value);
+}
 function gotosection(value)
 {
 	window.location.href = value;
@@ -14,20 +16,17 @@ function gotosection(value)
 // remove all the options and clear the drop down list
 function removeAllOptions(selectbox)
 {
-	var i;
-	for(i=selectbox.options.length-1;i>=0;i--)
-	{
-	selectbox.remove(i);
-	}
+	$(selectbox.children).each(function(){
+		$(this).remove();
+	});
 }
 
 // add one option to a drop down list
 function addOption(selectbox,text,value )
 {
-	var optn = document.createElement("OPTION");
-	optn.text = text;
-	optn.value = value;
-	selectbox.options.add(optn);
+	$(selectbox).append(
+        $('<option></option>').val(value).html(text)
+    );
 }
 
 // update the users preference that is stored in the database when the user switches
@@ -38,16 +37,17 @@ function show_ajax(viewchoice, client, projectpath, id_to_show, id_to_hide)
 			url: "/client/"+client+"/"+projectpath+"/",
 			data: {choice: viewchoice},
 			success: function(){
-				oldChoice = window.varChoice;
-				window.varChoice = viewchoice;
-				oldSection = window.varCurrentSection;
+				oldChoice = VAR_CHOICE;
+				VAR_CHOICE = viewchoice;
+				oldSection = VAR_CURRENT_SECTION;
 				$(id_to_hide).hide();
 				// open the newly selected grid or list view on the category/tab that was last viewed
-				gotosection('#'+window.currentCategory);
-				fillDropdown(window.currentCategory, window.varChoice);	
+				gotosection('#'+VAR_CURRENT_CATEGORY);
+				fillDropdown(VAR_CURRENT_CATEGORY, VAR_CHOICE);	
 				$(id_to_show).show();
 				// construct the jquery tools tabs
-				$("ul.tabs").tabs("div.panes"+viewchoice +" > div");								
+				$("ul.tabs").tabs("div.panes"+viewchoice +" > div");	
+				generateClickEvents();							
 				// jump to show the last viewed section unless it was the first section
 				// in which case it will be visible at the top of the page automatically, no need to jump to it
 				if(!(oldSection.indexOf('#1') === 0 && oldSection.charAt(2) === "_")){				
@@ -55,7 +55,7 @@ function show_ajax(viewchoice, client, projectpath, id_to_show, id_to_hide)
 					$('#select_section').val(newSection);
 					gotosection(newSection);
 					// save the oldSection because fillDropdown() has changed it
-					window.varCurrentSection = oldSection;
+					VAR_CURRENT_SECTION = oldSection;
 				}
 			}
 	});
